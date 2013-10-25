c5fa571bb5cf2016a4600c5fb28a5666f43d51d0
uS-aito <h2so40627@gmail.com>
2013-10-26 00:52:52 +0900

    Change Mirror-Rabin to the primality algorithm

--- findPrime.py
+++ findPrime.py
@@ -1,6 +1,7 @@
 ﻿#coding UTF-8
 import math
 import sys
+import random
 
 #試し割り関数
 def isPrime(num):
@@ -8,6 +9,8 @@ def isPrime(num):
 	
 	if num < 2:
 		return 0
+	if num == 2:
+		return 1
 	if num % 2 == 0:
 		return 0
 
@@ -21,6 +24,31 @@ def isPrime(num):
 	for var in TmpList:
 		if num % var == 0:
 			return 0
+			
+#ミラー･ラビンテスト
+def isPrime2(num,k=50):
+
+	if num < 2:
+		return 0
+	if num == 2:
+		return 1
+	if num % 2 == 0:
+		return 0
+
+	d = (num-1)>>1
+	while d % 2 == 0:
+		d >>= 1
+	
+	for i in xrange(k):
+		a = random.randint(1,num-1)
+		t = d
+		y = pow(a,t,num)
+		while not t == num-1 and not y == 1 and not y == num-1: 
+			y = pow(y,2,num)
+			t <<= 1
+		if not y == num-1 and t % 2 == 0:
+			return 0
+	return 1
 
 #変数群
 Pi = 1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
@@ -43,15 +71,15 @@ if not argvs[1].isdigit():
 
 digits = int(argvs[1])
 
-for var in range(0,len(strPi)):
+for var in xrange(0,len(strPi)):
 	PiList.append(int(strPi[var]))
 
 while 1:
 	obj = 0
-	for var in range(0,digits):
+	for var in xrange(0,digits):
 		obj = obj * 10 + PiList[position+var]
 	print "Trying "+str(obj)
-	if isPrime(obj) == 1:
+	if isPrime2(obj) == 1:
 		print obj
 		break
 	else:
